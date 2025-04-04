c01cedbacf65366da46ad945ec0e0ac9eba82858
Saksham <Sakshamsahu77783@gmail.com>
2025-04-04 17:35:52 +0530

    Navbar added

---
+++ navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-900 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-2xl font-bold">MyApp</div>
+        <ul className="flex space-x-6">
+          <li><Link to="/" className="hover:text-blue-400">Home</Link></li>
+          <li><Link to="/about" className="hover:text-blue-400">About</Link></li>
+          <li><Link to="/services" className="hover:text-blue-400">Services</Link></li>
+          <li><Link to="/contact" className="hover:text-blue-400">Contact</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
